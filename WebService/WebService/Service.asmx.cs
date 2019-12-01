a82375b05b901fac4fa2ba6ac5f31ca75fe05a5d
ksghir <sghir.kaltoum@gmail.com>
2019-12-01 12:52:14 +0100

    intial Commit

---
+++ WebService/WebService/Service.asmx.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+
+namespace WebService
+{
+    /// <summary>
+    /// Description résumée de Service
+    /// </summary>
+    [WebService()]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Pour autoriser l'appel de ce service Web depuis un script à l'aide d'ASP.NET AJAX, supprimez les marques de commentaire de la ligne suivante. 
+    //[System.Web.Script.Services.ScriptService]
+    public class Service : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int Fibonacci(int n)
+        {
+            if (n >= 1 && n <= 100)
+            {
+                int a = 0; // F0
+                int b = 1; // F1
+
+                for (int i = 0; i < n; i++)
+                {
+                    int tmp = a;
+                    a = b;
+                    b = tmp + b;
+                }
+
+                return a;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+
+        [WebMethod]
+        public string XmlToJson(string xml)
+        {
+            string json = "";
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xml);
+                json= JsonConvert.SerializeXmlNode(doc);
+            }
+            catch(Exception e)
+            {
+                json = "Bad Xml format";
+            }
+            return json;
+        }
+    }
+}
---
+++ winFormClient/WinFormClient/Form1.cs
@@ -0,0 +1,63 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WinFormClient
+{
+    public partial class Form1 : Form
+    {
+        private int response { get; set; }
+        private static readonly ILog Logger =
+            LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public Form1()
+        {
+            InitializeComponent();
+            Logger.Info("Start Application");
+        }
+    
+        public async Task<int> callService(int i)
+        {
+            try
+            { 
+            localhost.Service service = new localhost.Service();
+            Logger.Info("caling the service at "+service.Url);
+            int r =  service.Fibonacci(10);
+            Thread.Sleep(2000);
+            Logger.Info("Resposne is OK");
+            return r;
+            }
+            catch(Exception e)
+            {
+                Logger.Error("Error When calling the web Service " + e.Message);
+                return 0;
+            }
+        }
+
+        private async void callCalculateServcie()
+        {
+            Task<int> taskCalcule = callService(10);
+
+            response =  await taskCalcule;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (BusyForm frm = new BusyForm(callCalculateServcie))
+            {
+
+                frm.ShowDialog(this);
+                MessageBox.Show("Response : " + response);
+            }
+        }
+    }
+}
